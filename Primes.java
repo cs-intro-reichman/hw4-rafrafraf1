08c349854a693b930cea0a9c9761f718f9875ab9
Rafael Asmoucha <rasmoucha@gmail.com>
2024-11-27 18:55:29 +0200

    did primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,30 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+
+        boolean[] preems = new boolean[n+1];
+
+        for (int i = 0; i <= n; i++) { // default all positions as true
+            if (i >= 2) {
+                preems[i] = true;
+            }
+            else {
+                preems[i] = false; // 0 and 1 default to false obv
+            }
+        }
+        
+        System.out.println("Prime numbers up to " + n + ":");
+
+        int counter = 0;
+        for (int i = 0; i <= n; i++) {
+            if (preems[i]) { // we found a prime, very nice so now lets get rid of all it's multiples
+                for (int j = 2*i; j <= n; j += i) { // we start at the next multiple of the prime and increment by the prime
+                    preems[j] = false;
+                }
+                System.out.println(i);
+                counter++;
+            }
+        }
+        System.out.println("There are " + counter + " primes between 2 and " + n + ". (" + (int) (((double)counter / (double)n)*100) + "% are primes)");
     }
 }
